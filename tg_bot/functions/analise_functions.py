2f6848badc7842fb6388eb2bb51571c41d428dde
astronom-v-cube <astronom.v.cube@gmail.com>
2021-08-20 22:25:30 +0300

    Update

--- tg_bot/functions/analise_functions.py
+++ tg_bot/functions/analise_functions.py
@@ -4,7 +4,22 @@ import urllib3
 import requests
 from tqdm import tqdm
 from astro_bot_vars import *
-from functions.sending_functions import *
+from functions.db_functions import *
+from aiogram import Bot, types
+from secret_data import *
+
+bot = Bot(token = main_token, parse_mode = types.ParseMode.HTML)
+
+def sending(degree, degree_for_sender):
+
+    list_id = search_into_db(degree)
+
+    for id_one in list_id:
+
+        bot.send_message(chat_id = id_one, text = emojize(f':heavy_exclamation_mark: Внимание! График достиг уровня {degree_for_sender} :heavy_exclamation_mark:'), reply_markup = standart_keyboard)
+
+    print(f'Выполнил рассылку уровня {degree}')
+
 
 # функция проверки графика и высылания ответа с анализом
 def graphs_analise(degree, degree_for_sender):
--- tg_bot/functions/sending_functions.py
+++ tg_bot/functions/sending_functions.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, types
 from secret_data import *
+from functions.db_functions import *
 
 bot = Bot(token = main_token, parse_mode = types.ParseMode.HTML)
 
@@ -13,12 +14,12 @@ def send_attachment(user_id, image, keyboard):
     print(f'Ответил фото пользователю с id: {user_id}')
     return bot.send_photo(chat_id = user_id, photo = image, reply_markup = keyboard)
 
-def sending(degree, degree_for_sender):
-
-    list_id = search_into_db(degree)
-
-    for id_one in list_id:
-
-        bot.send_message(chat_id = id_one, text = emojize(f':heavy_exclamation_mark: Внимание! График достиг уровня {degree_for_sender} :heavy_exclamation_mark:'), reply_markup = standart_keyboard)
-
-    print(f'Выполнил рассылку уровня {degree}')
+# def sending(degree, degree_for_sender):
+#
+#     list_id = search_into_db(degree)
+#
+#     for id_one in list_id:
+#
+#         bot.send_message(chat_id = id_one, text = emojize(f':heavy_exclamation_mark: Внимание! График достиг уровня {degree_for_sender} :heavy_exclamation_mark:'), reply_markup = standart_keyboard)
+#
+#     print(f'Выполнил рассылку уровня {degree}')
